d99aaf1fbddd97a54ec919c6ef5f9093f14bc80b
Wholeheartedly <119653204+AESMatias@users.noreply.github.com>
2023-06-27 05:39:37 -0400

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,10 @@
+def selection_sort(list):
+  for i in range(len(list)-1):
+  position = i
+    for j in range(pos+1, len(list)):
+      if (list[pos] > list[j]):
+        position = j
+        aux = list[position]
+        list[position] = list[i]
+        list[i] = aux
+return list
