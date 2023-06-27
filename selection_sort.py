272dea6791ce680f5beb0df2b1e51e455e73cea6
Wholeheartedly <119653204+AESMatias@users.noreply.github.com>
2023-06-27 05:48:03 -0400

    Big tabulation mistake solved

--- selection_sort.py
+++ selection_sort.py
@@ -4,7 +4,7 @@ def selection_sort(list):
     for j in range(pos+1, len(list)):
       if (list[pos] > list[j]):
         position = j
-        aux = list[position]
-        list[position] = list[i]
-        list[i] = aux
+    aux = list[position]
+    list[position] = list[i]
+    list[i] = aux
 return list
